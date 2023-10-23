0916fe24753ee43dd43a4b069c5f99002e592b9a
gp974 <gp974@uowmail.edu.au>
2023-10-23 19:19:19 +0400

    .

--- Lab2-Q1.c
+++ Lab2-Q1.c
@@ -30,11 +30,23 @@ int main()
     set_array (arrPart1, SIZE);
     // print_array(arrPart1, SIZE);
 
-    //Part 5
-    int arrPart5[nRows][nCols];
-    print_trans_matrix(arrPart5[nRows][nCols]);
+    //Part 2
+    int arrPart2[SIZE];
+    set_array (arrPart2, SIZE);
+    rem_align (arrPart2, SIZE, -4);
+    print_array (arrPart2, SIZE);
 
-    //Part 6
+    //Part 3
+
+
+    //Part 4
+
+
+    //Part 5
+    int arrPart5[nRows][nCols] = {{1,2,3},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{7,8,9}};
+    // print_trans_matrix(arrPart5);
+
+    //Part 6 not complete but almost
     int arrPart6[SIZE]={1,2,5,4,9,10,6,8,4};
     int x = found_duplicate(arrPart6,SIZE);
     if (x==1){
@@ -63,39 +75,48 @@ void print_matrix(int mat[][nCols], int rows){
    }     
 }
 
+
 //Part 1
 void set_array(int arr [], int length){
     for(int i=0; i<length; i++)
         arr[i]=i;
 }
 
-/*
+//Part 2
 void rem_align(int arr[], int length, int pos){
-    int x;
-    for(int i=0; i<length; i++){
-        
-    }
+    if (pos>0){
+        for(int i=0; i<length; i++){
+            for (int j=pos; j<length; j++){
+                arr[j] = arr[j+i];
+            }
+        }
+    }else printf("Number should be positive.\n");
 }
 
-
-
+//Part 3
 void insert_align(int arr[], int length, int pos, int value){
-
+    
 }
 
+//Part 4
 void reshape(int arr[], int length, int arr2d[nRows][nCols]){
 
 }
-*/
+
 
 //Part 5
 void print_trans_matrix(int arr2d[nRows][nCols]){
-    int trans[][];
+    int transpose[100][100];
     for(int i=0; i<nRows; i++){
         for(int j=0; j<nCols; j++){
-            trans[j][i]=arr2d[i][j];
-            printf("%d ",trans[i][j]);
+            transpose[j][i]=arr2d[i][j];
         }
+    }
+    for(int i=0; i<nCols; i++){
+        for(int j=0; j<nRows; j++){
+            printf("%d  ",transpose[i][j]);
+        }
+    printf("\n");
     }
 }
 
